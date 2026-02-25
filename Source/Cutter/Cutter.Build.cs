2119b12cfa2e9170af3cf80d8d8ab080effd1fa4
Kyoka Ohnishi <kyoka.ohnishi@outlook.jp>
2026-02-25 14:17:08 +0900

    初期コミット

---
+++ Source/Cutter/Cutter.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Cutter : ModuleRules
+{
+	public Cutter(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Cutter",
+			"Cutter/Variant_Strategy",
+			"Cutter/Variant_Strategy/UI",
+			"Cutter/Variant_TwinStick",
+			"Cutter/Variant_TwinStick/AI",
+			"Cutter/Variant_TwinStick/Gameplay",
+			"Cutter/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Cutter/Cutter.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cutter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cutter, "Cutter" );
+
+DEFINE_LOG_CATEGORY(LogCutter)
+ 
